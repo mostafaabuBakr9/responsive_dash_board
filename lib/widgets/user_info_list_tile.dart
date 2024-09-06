7c1d261b8b6c350e638fb0b514c959b3f5559aa2
AbuBakr14 <mostafaabubakr9@gmail.com>
2024-09-06 15:41:58 +0300

    add user info list tile widgit

--- lib/widgets/user_info_list_tile.dart
+++ lib/widgets/user_info_list_tile.dart
@@ -1,14 +1,31 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:responsive_dash_board/utils/app_images.dart';
+
+import 'package:responsive_dash_board/utils/app_styles.dart';
 
 class UserInfoListTile extends StatelessWidget {
-  const UserInfoListTile({super.key});
-
+  const UserInfoListTile({
+    Key? key,
+    required this.title,
+    required this.subtitle,
+    required this.image,
+  });
+  final String image, title, subtitle;
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      leading: SvgPicture.asset(Assets.imagesAvatar1,),
+      leading: SvgPicture.asset(
+        image,
+      ),
+      title: Text(
+        title,
+        style: AppStyles.styleSemiBold16,
+      ),
+      subtitle: Text(
+        subtitle,
+        style: AppStyles.styleRegular12,
+      ),
     );
   }
 }
